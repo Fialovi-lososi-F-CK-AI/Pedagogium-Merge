3453f5bca8fa3db5bbef2720a01c4c238750abd0
Dvormen <dvorak13@spsejecna.cz>
2026-03-04 20:55:21 +0100

    fix(pull request): hopefully fixed issues with pulling

--- src/backend/src/Utils/helpers.php
+++ src/backend/src/Utils/helpers.php
@@ -5,16 +5,14 @@ namespace App\Utils;
  * Bezpečně převádí mixed na string nebo int s fallbackem
  *
  * @param mixed $value
- * @param 'string'|'int'|'float' $type
- * @param string|int|float|null $default
- * @return string|int|float
+ * @param 'string'|'int' $type
+ * @param string|int|null $default
+ * @return string|int
  */
-function cast(mixed $value, string $type, mixed $default = null): mixed
+function cast(mixed $value, string $type, mixed $default = null): string|int
 {
     return match($type) {
         'string' => is_scalar($value) ? (string) $value : (string) $default,
         'int' => is_numeric($value) ? (int) $value : (int) $default,
-        'float' => is_numeric($value) ? (float) $value : (float) $default,
-        default => $default,
     };
 }
